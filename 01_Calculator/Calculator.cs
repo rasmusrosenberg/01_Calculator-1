9936b0097ae1a4151fffcfba763c5be2c6938308
Lasse Jacobsen <lassejacobsen@mail.com>
2016-09-06 09:59:00 +0200

    Finished

--- 01_Calculator/Calculator.cs
+++ 01_Calculator/Calculator.cs
@@ -27,9 +27,50 @@ namespace _01_Calculator
 
             foreach (int number in numbers)
             {
-                sum = sum + number;
+                sum += number;
             }
 
+            return sum;
+        }
+
+        public static int Multiply(int[] numbers)
+        {
+            int sum = 1;
+
+            foreach (int number in numbers)
+            {
+                sum *= number;
+            }
+
+            return sum;
+        }
+
+        public static double Power(int arg1, int arg2)
+        {
+            //  Solution using Math namespace
+            //return Math.Pow(arg1, arg2);
+
+            int sum = 1;
+
+            for (int i = 1; i <= arg2; i++)
+            {
+                sum *= arg1;
+            }
+
+            return sum;
+        }
+
+        public static int Factorial(int arg)
+        {
+            if (arg <= 0)
+                return 1;
+
+            int sum = 1;
+            for (int i = 0; i < arg; i++)
+            {
+                sum *= arg - i;
+            }
+            
             return sum;
         }
     }
--- 01_Calculator/CalculatorTest.cs
+++ 01_Calculator/CalculatorTest.cs
@@ -36,28 +36,34 @@ namespace _01_Calculator
         [TestMethod]
         public void Multiply() {
 
-            //It multiplies two numbers
-            //It multiplies several numbers
-            int[] numbers = new int[2];
+            //It multiplies two 
+            int[] numbers = new int[] { 7, 11 };
+            Assert.AreEqual(77, Calculator.Multiply(numbers));
 
-            throw new NotImplementedException();
+            //It multiplies several numbers
+            int[] arguments = new int[] { 7, 11, 5, 8 };
+            Assert.AreEqual(3080, Calculator.Multiply(arguments));
         }
         [TestMethod]
         public void Power()
         {
             //It raises one number to the power of another number
-            throw new NotImplementedException();
+            Assert.AreEqual(216, Calculator.Power(6, 3));
         }
         [TestMethod]
         public void Factorial()
         {
             //# http://en.wikipedia.org/wiki/Factorial
             //  it "computes the factorial of 0"
+            Assert.AreEqual(1, Calculator.Factorial(0));
             //  it "computes the factorial of 1"
+            Assert.AreEqual(1, Calculator.Factorial(1));
             //  it "computes the factorial of 2"
+            Assert.AreEqual(2, Calculator.Factorial(2));
             //  it "computes the factorial of 5"
+            Assert.AreEqual(120, Calculator.Factorial(5));
             //  it "computes the factorial of 10"
-            throw new NotImplementedException();
+            Assert.AreEqual(3628800, Calculator.Factorial(10));
         }
     }
 }
